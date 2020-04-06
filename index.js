b897568dac8dff1656b8522338204575a2914d4f
Natalia Beckstead <toosmiravilla@gmail.com>
2020-04-06 13:35:46 -0600

    set up server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.json({api: 'running.....'});
+});
+
+const port = 5000;
+server.listen(port, () => console.log(`\n== api on port ${port} ==\n`));
