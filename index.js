5c638eb23513efff634172731c2160b71170a84a
Natalia Beckstead <toosmiravilla@gmail.com>
2020-04-06 14:02:03 -0600

    get and post end points

--- index.js
+++ index.js
@@ -3,8 +3,36 @@ const server = express();
 
 server.use(express.json());
 
+let users = [
+    {
+        id: 1, 
+        name: "Jane Doe", 
+        bio: "Not Tarzan's Wife, another Jane"
+    }
+]
+
 server.get('/', (req, res) => {
     res.json({api: 'running.....'});
+});
+
+server.get('/api/users', (req, res) => {
+    res.json(users);
+});
+
+server.post('/api/users', (req, res) => {
+    const userInfo = req.body;
+    users.push(userInfo);
+    res.status(201).json(users);
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users.find((user) => user.id == id);
+    if (user) {
+        res.status(200).json(user);
+    } else {
+        res.status(404).json({ message: "User not found"});
+    }
 });
 
 const port = 5000;
