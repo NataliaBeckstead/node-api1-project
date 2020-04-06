2c1f25b871d8b1abc5f2b139063eea7a0b2fc963
Natalia Beckstead <toosmiravilla@gmail.com>
2020-04-06 14:40:55 -0600

    delete request

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ let users = [
         id: 1, 
         name: "Jane Doe", 
         bio: "Not Tarzan's Wife, another Jane"
+    },
+    {
+        id: 2,
+        name: "Cheburashka",
+        bio: "Was found in oranges"
+    },
+    {
+        id: 3,
+        name: "Baba Yaga",
+        bio: "Lives in a chicken-leg house in the middle of the forest"
     }
 ]
 
@@ -26,13 +36,23 @@ server.post('/api/users', (req, res) => {
 });
 
 server.get('/api/users/:id', (req, res) => {
-    const id = req.params.id;
-    const user = users.find((user) => user.id == id);
+    const userId = req.params.id;
+    const user = users.find((user) => user.id == userId);
     if (user) {
         res.status(200).json(user);
     } else {
         res.status(404).json({ message: "User not found"});
     }
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    const reqId = req.params.id;
+    let user = users.filter(user => {
+        return user.id == reqId;
+    })[0];
+    const index = users.indexOf(user);
+    users.splice(index, 1);
+    res.json(users);
 });
 
 const port = 5000;
